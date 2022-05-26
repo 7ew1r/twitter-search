153cb9428d678b1a1e57ae02a5dd22533dde1e23
7ew1r <art.sht.0317@gmail.com>
2022-05-26 11:41:43 +0900

    add alpine.js

--- js/script.js
+++ js/script.js
@@ -1,96 +1,97 @@
-flatpickr("#since", {});
-flatpickr("#until", {});
-
-const formNodes = {
-  "keyword": document.getElementById("keyword"),
-  "since": document.getElementById("since"),
-  "until": document.getElementById("until"),
-  "min-faves": document.getElementById("min-faves"),
-  "min-retweets": document.getElementById("min-retweets"),
+const isDate = (str) => {
+  return !(str === undefined || str === null || str === "");
 };
 
-const imageFilterNode = document.getElementById("image-filter");
-const videoFilterNode = document.getElementById("video-filter");
-const langNode = document.getElementById("lang");
-
-
-const keyword = () => {
-  return formNodes["keyword"].value
-}
-
-const since = () => {
-  if (!formNodes["since"].value) {
+const filteredQuery = (value, item) => {
+  if (value === "") {
+    return ""
+  } else if (value === "include") {
+    return `filter:${item}`;
+  } else if (value === "exclude") {
+    return `-filter:${item}`;
+  } else {
     return "";
   }
-  const since = new Date(formNodes["since"].value);
-  formNodes["since"]
-  return `since:${since.getFullYear()}-${since.getMonth() + 1}-${since.getDate()}`;
-};
-
-const until = () => {
-  if (!formNodes["until"].value) {
-    return "";
-  }
-  const until = new Date(formNodes["until"].value);
-  return `until:${until.getFullYear()}-${until.getMonth() + 1}-${until.getDate()}`;
-};
-
-const minFaves = () => {
-  if (!formNodes["min-faves"].value) {
-    return "";
-  }
-  return `min_faves:${formNodes["min-faves"].value}`;
 }
 
-const minRetweets = () => {
-  if (!formNodes["min-retweets"].value) {
-    return "";
-  }
-  return `min_retweets:${formNodes["min-retweets"].value}`;
+function app() {
+  return {
+    keyword: "",
+    since: "",
+    until: "",
+    minFaves: "",
+    minRetweets: "",
+    lang: "",
+    imagesFilter: "",
+    videosFilter: "",
+    linksFilter: "",
+    verifiedFilter: "",
+    isUntilBeforeSince() {
+      if (!isDate(this.until) || !isDate(this.since)) {
+        return false;
+      }
+      const since = new Date(this.since);
+      const until = new Date(this.until);
+      return until < since;
+    },
+    isValidUrl() {
+      return this.keyword !== "" && !this.isUntilBeforeSince();
+    },
+    sinceQuery() {
+      if (!isDate(this.since)) {
+        return ""
+      }
+      const since = new Date(this.since);
+      return `since:${since.getFullYear()}-${since.getMonth() + 1}-${since.getDate()}`;
+    },
+    untilQuery() {
+      if (!isDate(this.until)) {
+        return ""
+      }
+      const until = new Date(this.until);
+      return `until:${until.getFullYear()}-${until.getMonth() + 1}-${until.getDate()}`;
+    },
+    minFavesQuery() {
+      if (this.minFaves === "") {
+        return "";
+      }
+      return `min_faves:${this.minFaves}`;
+    },
+    minRetweetsQuery() {
+      if (this.minRetweets === "") {
+        return "";
+      }
+      return `min_retweets:${this.minRetweets}`;
+    },
+    languageQuery() {
+      if (this.lang === "") {
+        return ""
+      }
+      return `lang:${this.lang}`;
+    },
+    imageFilterQuery() {
+      return filteredQuery(this.imagesFilter, "images");
+    },
+    videoFilterQuery() {
+      return filteredQuery(this.videosFilter, "videos");
+    },
+    linkFilterQuery() {
+      return filteredQuery(this.linksFilter, "links");
+    },
+    verifiedFilterQuery() {
+      return filteredQuery(this.verifiedFilter, "verified");
+    },
+    createQuery() {
+      return [this.sinceQuery(), this.untilQuery(), this.minFavesQuery(), this.minRetweetsQuery(), this.keyword, this.languageQuery(), this.imageFilterQuery(), this.videoFilterQuery(), this.linkFilterQuery(), this.verifiedFilterQuery()].filter(v => v).join(" ");
+      //return [this.since(), this.until(), keyword(), imageFilter(), videoFilter(), language(), minFaves(), minRetweets()].filter(v => v).join(" ");
+    },
+    createUrl() {
+      const baseUrl = "https://twitter.com/search";
+      const query = this.createQuery();
+      return encodeURI(baseUrl + "?q=" + query);
+    },
+    openUrl() {
+      window.open(this.createUrl());
+    }
+  };
 }
-
-const language = () => {
-  return langNode.value === "" ? "": `lang:${langNode.value}`;
-}
-
-const imageFilter = () => {
-  if (imageFilterNode.checked) {
-    return "filter:images";
-  }
-  return "";
-}
-
-const videoFilter = () => {
-  if (videoFilterNode.checked) {
-    return "filter:videos";
-  }
-  return "";
-}
-
-
-const createQuery = () => {
-  return [since(), until(), keyword(), imageFilter(), videoFilter(), language(), minFaves(), minRetweets()].filter(v => v).join(" ");
-}
-
-const createUrl = () => {
-  const baseUrl = "https://twitter.com/search";
-  const query = createQuery();
-  return encodeURI(baseUrl + "?q=" + query);
-};
-
-const updateUrl = (e) => {
-  const newUrl = createUrl();
-  const url = document.getElementById("url");
-  url.value = newUrl;
-  const searchButton = document.getElementById("search_button");
-  searchButton.href = newUrl;
-}
-
-Object.keys(formNodes).forEach((node) => {
-  formNodes[node].addEventListener("input", updateUrl);
-});
-
-imageFilterNode.addEventListener("change", updateUrl);
-videoFilterNode.addEventListener("change", updateUrl);
-langNode.addEventListener("change", updateUrl);
-
